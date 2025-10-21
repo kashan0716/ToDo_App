78ac0a43b2c9c3f187eb40a1b8dc976220ae7a08
Mohd Kashan <kashan0716@gmail.com>
2025-10-21 20:20:24 +0530

    sas

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { taskRouter } from "./routes/task.route.js";
+import { taskRouter } from "./src/routes/task.route.js";
 import mongoose from "mongoose";
 
 const app = express();
